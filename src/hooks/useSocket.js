73b5c8a08840d56176819c4fbec2cea1104d58e2
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2025-03-17 13:30:23 +0000

    removed api route

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = "http://34.220.153.237:3000"; // Replace with your EC2 instance IP
 
 export const useSocket = () => {
   const [socket, setSocket] = useState(null);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    const newSocket = io(SOCKET_SERVER_URL, { transports: ["websocket", "polling"] });
+    const newSocket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL, { transports: ["websocket", "polling"] });
     
     newSocket.on("connect", () => {
       console.log("Connected to WebSocket Server");
