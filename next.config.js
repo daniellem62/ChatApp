ecb43791fc8d91c06af3e6063c2b09d675fab0e1
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2025-03-17 18:00:36 +0000

    and again

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Remove the redirects or point to a page that actually exists
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       destination: '/home',
-  //       permanent: true,
-  //     },
-  //   ]
-  // },
 }
 
-module.exports = nextConfig
+export default nextConfig;
