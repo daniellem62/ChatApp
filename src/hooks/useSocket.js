f47a55c6b87eaff014946ac7003b34ca8150680d
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2025-03-17 18:04:32 +0000

    Revert "got chat working again"
    This reverts commit ad0206bb3d482e6a1e6eee7f3e05ceaba639e586.

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,39 +1,19 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
+
 export const useSocket = () => {
   const [socket, setSocket] = useState(null);
   const [messages, setMessages] = useState([]);
-  const [connected, setConnected] = useState(false);
 
   useEffect(() => {
-    // Get the server URL from environment variable
-    const serverUrl =
-      process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || "http://localhost:3000";
-    console.log("Connecting to socket server at:", serverUrl);
-
-    const newSocket = io(serverUrl, {
-      transports: ["websocket", "polling"],
-      secure: serverUrl.startsWith("https"),
-    });
-
+    const newSocket = io(process.env.NEXT_PUBLIC_SOCKET_SERVER_URL, { transports: ["websocket", "polling"] });
+    
     newSocket.on("connect", () => {
       console.log("Connected to WebSocket Server");
-      setConnected(true);
-    });
-
-    newSocket.on("disconnect", () => {
-      console.log("Disconnected from WebSocket Server");
-      setConnected(false);
-    });
-
-    newSocket.on("connect_error", (err) => {
-      console.error("Connection error:", err);
-      setConnected(false);
     });
 
     newSocket.on("chat message", (msg) => {
-      console.log("Received message:", msg);
       setMessages((prevMessages) => [...prevMessages, msg]);
     });
 
@@ -43,15 +23,10 @@ export const useSocket = () => {
   }, []);
 
   const sendMessage = (msg) => {
-    if (socket && socket.connected) {
-      console.log("Sending message:", msg);
+    if (socket) {
       socket.emit("chat message", msg);
-      return true;
-    } else {
-      console.error("Cannot send message: socket not connected");
-      return false;
     }
   };
 
-  return { messages, sendMessage, connected };
+  return { messages, sendMessage };
 };
